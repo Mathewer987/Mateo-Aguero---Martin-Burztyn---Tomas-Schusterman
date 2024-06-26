61187b41fd6d4528ca934e56e64464560ae11f52
Martin_Burs <playmartu18@gmail.com>
2024-06-26 16:06:49 -0300

    empezando con las funciones iniciales del juegi
    que aparezcan los objetos escondidos al comienzo del juego

---
+++ Assets/Scripts/ProductPrices.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductPrices : MonoBehaviour
+{
+    public int precio;
+    void Start()
+    {
+        Ocultar();
+    }
+    public void Ocultar()
+    {
+        gameObject.SetActive(false);
+    }
+}
